e678e553620da8cf050ff6593c08ec8fc3ad15dd
edyother <ed@edyother.com>
2013-08-10 16:40:35 -0400

    update

--- rocatpdf.sh
+++ rocatpdf.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Rotates a directory of pdf files 90 degrees counterclockwise and merges them into one pdf file
+
+# Requires imagemagick and pdfunite
+
+# ./rocatpdf newfilename.pdf
+
 mkdir -p rotated
 
-for i in $( ls ); do 
+for i in $( ls *.pdf ); do 
 	convert -rotate 270 $i ./rotated/$i
 done
 
